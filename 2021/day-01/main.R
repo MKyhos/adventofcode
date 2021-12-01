0638251e2330ac6b4746942bc95f5842426d5177
Malte Kyhos <malte.kyhos@civey.com>
2021-12-01 23:00:25 +0100

    Update code, day 1

--- 2021/day-01/main.R
+++ 2021/day-01/main.R
@@ -1,22 +1,23 @@
 #' Advent of Code, 2021 | Day 1
 
-input <- as.integer(readLines("input.txt"))
-input <- input[!is.na(input)]
+input <- scan("input.txt")
+
+# Part I:
+print(sum(diff(input) > 0))
+
+# Part II:
 
 rollsum <- function(vec, s = 3) {
-  vapply(
-    X = s:length(vec),
-    FUN = function(k) sum(vec[(k - s + 1):k]),
-    FUN.VALUE = numeric(1)
-  )
+  m <- length(vec) - s + 1
+  d <- vec[1:s + rep.int(1:m, rep.int(s, m)) - 1]
+  dim(d) <- c(s, m)
+  colSums(d)
 }
 
 f <- function(vec) {
-
   if (TRUE)
     vec <- rollsum(vec, s = 3)
   sum(diff(vec) > 0)
 }
 
 print(f(input))
-
