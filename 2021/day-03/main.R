897a41084e6951d5b8d026d8511b3b26adf16bf8
Malte Kyhos <malte.kyhos@civey.com>
2021-12-07 12:46:37 +0100

    Add day 3 (part II) in R

--- 2021/day-03/main.R
+++ 2021/day-03/main.R
@@ -9,6 +9,7 @@ mat <- matrix(mat, nrow = 1000, ncol = 12, byrow = TRUE)
 bin <- (colSums(mat) / nrow(mat)) >= 0.5
 
 bin2decimal <- function(bin) {
+  bin <- as.logical(bin)
   pow <- 2 ** ((length(bin) - 1):0)
   sum(pow[bin])
 }
@@ -18,3 +19,35 @@ epsilon <- bin2decimal(!bin)
 print(gamma * epsilon)
 
 # Part II:
+
+find_vals <- function(mat, rule = "oxygen") {
+  for (j in seq_len(ncol(mat))) {
+    freq_one <- sum(mat[, j]) / nrow(mat)
+    if (freq_one > 0.5)
+      common <- 1
+    else if (freq_one < 0.5)
+      common <- 0
+    else
+      common <- -1
+
+    filter_val <- abs(common)
+    if (rule == "co2")
+      filter_val <- as.numeric(!common)
+
+    msg <- sprintf("J: %s, rows: %s, freq_one: %s, filter: %s.", j, nrow(mat), freq_one, filter_val)
+    message(msg)
+    subsetter <- mat[, j] == filter_val
+
+    mat <- mat[subsetter, ]
+
+    if (sum(subsetter) == 1)
+      return(mat)
+
+  }
+  return(mat)
+}
+
+oxygen <- bin2decimal(find_vals(mat, "oxygen"))
+co2 <- bin2decimal(find_vals(mat, "co2"))
+print(oxygen * co2)
+
