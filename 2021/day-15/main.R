4239772db8209e5a136ea357a6da23ba58419374
Malte Kyhos <malte.kyhos@civey.com>
2021-12-17 01:16:01 +0100

    Add solution for day 15

---
+++ 2021/day-15/main.R
@@ -0,0 +1,50 @@
+
+dim <- 100
+input <- matrix(
+  scan(
+    pipe("awk '{gsub(\"\", \" \", $1); print $1;}' input.txt"),
+    what = integer()
+  ),
+  nrow = dim,
+  byrow = TRUE
+)
+
+# Rotate Matrix to make it easier
+
+minimumPath <- function(mat, n ,m, incr = 0) {
+  st <- matrix(0 , nrow = n, ncol = m)
+  for (i in 2:n)
+    st[i, 1] <- st[i - 1, 1] + mat[i, 1]
+
+  for (j in 2:m)
+    st[1, j] <- st[1, j - 1] + mat[1, j]
+
+  for (i in 2:n) {
+    for (j in 2:m)
+      st[i, j] <- mat[i, j] + min(st[i, j - 1], st[i - 1, j])
+  }
+  return(st[n, m])
+}
+
+minimumPath(input, dim, dim)
+
+
+# Construct larger map
+
+# We can use the base function to construct a Kronecker sum, by first defining
+# the summation matrix that gives the amount that every submatrics should get
+# added.
+# Afterwards, we have to assure that the values fall into 1, ..., 9. For doing
+# so, one can apply Modulo 9 and subsequently "fix" the 0s back to 9s.
+
+sum_mat <- matrix(rep(0:4, 5) + rep(0:4, each = 5), byrow = TRUE, nrow = 5)
+
+input2 <- kronecker(sum_mat, input, FUN = "+") %% 9
+input2[input2 == 0] <- 9
+
+
+# Test input passes, however main input does not...
+
+minimumPath(input2, dim * 5, dim * 5)
+
+
